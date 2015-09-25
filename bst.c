ce7893913d34938dd8dda3131ad1504828beb00b
orodrigu <oswinrods@gmail.com>
2015-09-25 14:28:53 -0400

    additional updates

--- bst.c
+++ bst.c
@@ -13,31 +13,44 @@ size_t bst_size( bst_t *tree ) {
 }
 
 bool bst_insert( bst_t *tree, S32 val ) {	
+	// Initially point to root node
 	bsn_t *p_current = tree->root;
-	bsn_t new_node;
-	new_node.val = val;
-	new_node->left = new_node->right = NULL;
 	
+	// Initialize new node in tree
+	bsn_t *p_new_node = (bsn_t *)malloc(sizeof(bsn_t));
+	p_new_node->val = val;
+	p_new_node->left = p_new_node->right = NULL;
+	
+	// Scenario 1: empty tree
 	if(tree->size == 0){
-		tree->root = &new_node;
-		tree->size = 1;
+		// Root node is the new node
+		tree->root = p_new_node;
+		tree->size += 1;
 		return __TRUE;
 	}
+
+	// Scenario 2: non-empty tree; check if val already exists in tree
 	while(p_current->val != val){
+		// Check if must insert into left sub-tree
 		if(val < p_current->val){
+			// Check if left sub-tree is empty or not
 			if(p_current->left == NULL){
-				p_current->left = &new_node;
-				tree->size = tree->size + 1;
+				// Set left child as new node
+				p_current->left = p_new_node;
+				tree->size += 1;
 				return __TRUE;
 			}
 			else{
 				p_current = p_current->left;
 			}
 		}
+		// Else insert into right sub-tree
 		else{
+			// Check if right sub-tree is empty or not
 			if(p_current->right == NULL){
-				p_current->right = &new_node;
-				tree->size = tree->size + 1;
+				// Set right child as new node
+				p_current->right = p_new_node;
+				tree->size += 1;
 				return __TRUE;
 			}
 			else{
@@ -45,77 +58,97 @@ bool bst_insert( bst_t *tree, S32 val ) {
 			}
 		}
 	}
+
+	// Value already exists
 	return __FALSE;
 }
 
 S32 bst_min( bst_t *tree ) {
+	// Initially point to root node
 	bsn_t *p_current = tree->root;
 	
+	// Empty tree
 	if(tree->size == 0){
 		return INT_MAX;
 	}
+	// Keep traversing to the left-most node
 	while(p_current->left != NULL){
 		p_current = p_current->left;
 	}
+
 	return p_current->val;
 }
 
 S32 bst_max( bst_t *tree ) {
+	// Initially point to root node
 	bsn_t *p_current = tree->root;
 	
+	// Empty tree
 	if(tree->size == 0){
 		return INT_MIN;
 	}
+	// Keep traversing to the right-most node
 	while(p_current->right != NULL){
 		p_current = p_current->right;
 	}
+	
 	return p_current->val;
 }
 
 bool bst_erase( bst_t *tree, S32 val ) {
+	// Initially point to root node
 	bsn_t *p_current = tree->root;
-	bsn_t *p_previous = NULL;
+	// Initialize pointer to parent node of node-to-erase
+	bsn_t *p_parent = NULL;
 	
+	// Empty tree
 	if(tree->size == 0){
 		return __FALSE;
 	}
 	
-	while(p_current->val != val && p_current != NULL){
+	// Locate node-to-erase
+	while(p_current != NULL && p_current->val != val){
+		// Update parent node
+		p_parent = p_current;
+		// Which direction to traverse, depending on val
 		if(val < p_current->val){
-			p_previous = p_current;
 			p_current = p_current->left;
 		}
 		else{
-			p_previous = p_current;
 			p_current = p_current->right;
 		}
 	}
+
+	// Return false if val not found in tree
 	if(p_current == NULL){
 		return __FALSE;
 	}
 	
-	//Scenario 1 Leaf Node
+	// Scenario 1: leaf node
 	if(p_current->left == NULL && p_current->right == NULL){
+		// In the case that it is the only node in the tree
 		if(p_current == tree->root){
+			tree->root = NULL;
 			free(p_current);
-			p_current = tree->root = NULL;
-			tree->size = 0;
+			p_current = NULL;
+			tree->size -= 1;
 		}
 		else{
-			if(p_current->val < p_previous->val){
-				p_previous->left = NULL;
+			// Check which child of parent is the current node - left or right?
+			if(p_current->val < p_parent->val){
+				p_parent->left = NULL;
 			}
 			else{
-				p_previous->right = NULL;
+				p_parent->right = NULL;
 			}
+			free(p_current);
+			p_current = NULL;
+			tree->size -= 1;
 		}
-		free(p_current);
-		p_current = NULL;
-		tree->size = tree->size - 1;
 		return __TRUE;
 	}
 	
-	//Scenario 2 One Child
+	// Scenario 2: one child
 	else if (p_current->left == NULL || p_current->right == NULL){
 		if(p_current = tree->root){
 			if(p_current->left != NULL){
@@ -129,20 +162,20 @@ bool bst_erase( bst_t *tree, S32 val ) {
 			return __TRUE;
 		}
 		else{
-			if(p_current->val < p_previous->val){
+			if(p_current->val < p_parent->val){
 				if(p_current->left != NULL){
-					p_previous->left = p_current->left;
+					p_parent->left = p_current->left;
 				}
 				else{
-					p_previous->left = p_current->right;
+					p_parent->left = p_current->right;
 				}	
 			}
 			else{
 				if(p_current->left != NULL){
-					p_previous->right = p_current->left;
+					p_parent->right = p_current->left;
 				}
 				else{
-					p_previous->right = p_current->right;
+					p_parent->right = p_current->right;
 				}
 			}
 			free(p_current);
