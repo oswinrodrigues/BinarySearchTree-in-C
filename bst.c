f6bfb4f4ee1dac2fdfc453efa37695cda91be7bc
orodrigu <oswinrods@gmail.com>
2015-09-25 15:18:17 -0400

    Finished erase function

--- bst.c
+++ bst.c
@@ -100,7 +100,13 @@ bool bst_erase( bst_t *tree, S32 val ) {
 	bsn_t *p_current = tree->root;
 	// Initialize pointer to parent node of node-to-erase
 	bsn_t *p_parent = NULL;
-	
+	// Need another pointer for when node-to-erase has two sub-trees, in which case
+	// you can either get the greatest value in the left sub-tree or the smallest
+	// in the right sub-tree; let's call this 'offspring'. 
+	bsn_t *p_offspring = NULL;
+	// We will also need a pointer to the offspring's parent
+	bsn_t *p_offspring_parent = NULL;
+
 	// Empty tree
 	if(tree->size == 0){
 		return __FALSE;
@@ -124,7 +130,7 @@ bool bst_erase( bst_t *tree, S32 val ) {
 		return __FALSE;
 	}
 	
-	// Scenario 1: leaf node
+	// Scenario 1: node-to-erase is leaf node
 	if(p_current->left == NULL && p_current->right == NULL){
 		// In the case that it is the only node in the tree
 		if(p_current == tree->root){
@@ -148,42 +154,99 @@ bool bst_erase( bst_t *tree, S32 val ) {
 		return __TRUE;
 	}
 	
-	// Scenario 2: one child
+	// Scenario 2: node-to-erase has one sub-tree
+	// Given the previous 'if' condition, the following condition
+	// is only TRUE when ONLY ONE of the two children is NULL
 	else if (p_current->left == NULL || p_current->right == NULL){
+		// In the case that the root node is to be erased
 		if(p_current = tree->root){
+			// Update new root node of tree
 			if(p_current->left != NULL){
 				tree->root = p_current->left;
+				p_current->left = NULL;
 			}
 			else{
 				tree->root = p_current->right;
+				p_current->right = NULL;
 			}
 			free(p_current);
-			tree->size = tree->size - 1;
+			tree->size -= 1;
 			return __TRUE;
 		}
 		else{
+			// Check which child of parent is the current node - left or right?
 			if(p_current->val < p_parent->val){
+				// Update child of current to now be a child of parent
 				if(p_current->left != NULL){
 					p_parent->left = p_current->left;
+					p_current->left = NULL;
 				}
 				else{
 					p_parent->left = p_current->right;
+					p_current->right = NULL;
 				}	
 			}
 			else{
+				// Update child of current to now be a child of parent
 				if(p_current->left != NULL){
 					p_parent->right = p_current->left;
+					p_current->left = NULL;
 				}
 				else{
 					p_parent->right = p_current->right;
+					p_current->right = NULL;
 				}
 			}
 			free(p_current);
-			tree->size = tree->size - 1;
+			tree->size -= 1;
 			return __TRUE;	
 		}
 	}
 	
-	//Scenario 3
+	//Scenario 3: node-to-erase has two sub-trees
+	else {
+		// Choose to traverse to and retrive node with greatest value in left sub-tree
+		p_offspring = p_current->left;
+		p_offspring_parent = p_current;
+		while(p_offspring->right != NULL){
+			p_offspring_parent = p_offspring;
+			p_offspring = p_offspring->right;
+		}
+
+		// p_offspring will either have only a left child or none, so we adjust its
+		// parent's children accordingly
+		if (p_offspring_parent->left == p_offspring) {
+			p_offspring_parent->left = p_offspring->left;
+		}
+		else {
+			p_offspring_parent->right = p_offspring->left;
+		}
+
+		// Now set p_offspring's children
+		p_offspring->left = p_current->left;
+		p_offspring->right = p_current->right;
+
+
+		// Now adjust p_parent's children; there's two cases - p_current is either the
+		// root node or not
+		// In the case that the node-to-erase is the root node
+		if(p_current == tree->root){
+		    tree->root = p_offspring;
+		}
+		else {
+			if (p_parent->left == p_current) {
+				p_parent->left = p_offspring;
+			}
+			else {
+				p_parent->right = p_offspring;
+			} 
+		}
+
+		// Finally we free the node
+		free(p_current);
+		tree->size -= 1;
+		return __TRUE;
+	}
+
 	return __FALSE;
 }
